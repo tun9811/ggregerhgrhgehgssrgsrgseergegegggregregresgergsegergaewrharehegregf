3b7aedf848c30db4b35cea11b8937e3bb0c0ae3f
x3sharkx2z <bevz91741@gmail.com>
2024-12-29 14:59:21 +0700

    Update and rename README.md to Main.lua

---
+++ Main.lua
@@ -0,0 +1,25 @@
+local httpService = game:GetService("HttpService")
+
+local main = {} do
+	main.Folder = "FluentSettings"
+    main.General1 = {
+    local isAutoFarmActive = false
+
+local function autofarm()
+    while isAutoFarmActive do
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/tun9811/jilhihjikkguhjl/refs/heads/main/test.lua"))()
+	end
+end	
+
+local AutoFarmTogggle = Tabs.General1:AddToggle("AutoFarmTogggle", { Title = "Auto Farm Level", Description = "", Default = false })
+AutoFarmTogggle:OnChanged(function()
+    isAutoFarmActive = AutoFarmTogggle.Value
+	if AutoFarmTogggle then
+	    coroutine.wrap(autofarm)()
+	end
+end)
+    
+Options.AutoFarmTogggle.SetValue(false)
+
+    
+return InterfaceManager
